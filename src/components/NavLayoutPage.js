97fd9ba6460a7e9a3584ae0a80f27950461d8301
ctokoli <matumasie@gmail.com>
2023-07-28 13:45:05 +0000

    Update NavLayoutPage.js

--- src/components/NavLayoutPage.js
+++ src/components/NavLayoutPage.js
@@ -7,6 +7,7 @@ const NavLayout = () => (
         <li className="logo"><NavLink to="/">Bookstore CMS</NavLink></li>
         <li className="link"><NavLink to="/" className={({ isActive }) => (isActive ? 'active-link' : '')}>BOOKS</NavLink></li>
         <li className="link"><NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to="/categories">CATEGORIES</NavLink></li>
+        <li className="profile"><i className="fa-solid fa-user"> </i></li>
       </ul>
     </nav>
     <Outlet />
